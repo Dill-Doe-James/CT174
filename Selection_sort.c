b683da2ef2ee32f438a3308e8a5efa1b21b16d4a
Dill-Doe-James <hungb2303818@student.ctu.edu.vn>
2025-02-24 20:44:48 +0700

    Add files via upload

--- Selection_sort.c
+++ Selection_sort.c
@@ -1,80 +1,24 @@
 #include <stdio.h>
-typedef int keytype;
-typedef float othertype;
-typedef struct{
-	keytype key;
-	othertype otherfields;
-}recordtype;
-void swap(recordtype *x, recordtype *y){
-	recordtype temp;
-	temp = *x;
-	*x = *y;
-	*y = temp;
-}
-void Selection_sort(recordtype a[], int n){
-	int i,j,lowindex;
-	keytype lowkey;
-	for(i=0;i<=n-2;i++){
-		lowkey = a[i].key;
-		lowindex = i;
-		for(j=i+1;j<=n-1;j++){
-			if(a[j].key<lowkey){
-				lowkey = a[j].key;
-				lowindex = j;
-			}			
-		}
-	swap(&a[i],&a[lowindex]);	
-	}
-}
-void read_data(recordtype a[], int *n){
-	FILE *file;
-	file = fopen("data.txt","r");
-	int i=0;
-	if(file != NULL){
-		while(!feof(file)){
-			fscanf(file,"%d%f", &a[i].key, &a[i].otherfields);
-			i++;
-		}
-	}
-	else printf("Loi mo file!\n");
-	fclose(file);
-	*n = i;
-	
-}
 
-void insertionSort(recordtype a[], int n){
-	int i;
-	for(i=1;i<=n;i++){
-		int j = i;
-		while(j>0 && a[j].key < a[j-1].key){
-			swap(&a[j],&a[j-1]);
-			j--;
-		}
-	}
-}
-void BubbleSort(recordtype a[], int n){
-	int i,j;
-	for(i=0;i<=n-2;i++){
-		for(j=n-1;j>=i+1;j--){
-			if(a[j].key<a[j-1].key)
-				swap(&a[j].key, &a[j-1].key);
-		}
-	}
-}
-void print_data(recordtype a[], int n){
-	int i;
-	for(i=0;i<n;i++){
-		printf("%3d%5d%8.2f\n", i+1, a[i].key, a[i].otherfields);
-	}
-}
 int main(){
-	recordtype a[100];
-	int n=0;
-	read_data(a,&n);
-	printf("du lieu truoc khi xep:\n");
-	print_data(a,n);
-	BubbleSort(a,n);
-	printf("du lieu sau khi xep:\n");
-	print_data(a,n);
-	
+	int a[] = {4,5,2,1,6,7,3,9,8,0};
+	int length = 10;
+	int i,j;
+	for(i=0;i<length-1;i++){
+		int min_pos = i;
+		for(j=i+1;j<length;j++){
+			if(a[j] < a[min_pos]){
+				min_pos = j;
+			}
+		}
+		if(i!=min_pos){
+			int temp = a[i];
+			a[i] = a[min_pos];
+			a[min_pos] = temp;
+		}
+	}
+	for(i=0;i<length;i++){
+		printf("a[%d] = %d\n", i, a[i]);
+	}
+	return 0;
 }
